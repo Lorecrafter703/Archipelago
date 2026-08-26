166e949af0992477ebb836fa1bfd530a50ab07bd
Lorecrafter703 <danielzeman703@gmail.com>
2026-08-26 17:45:52 -0500

    Added baseline support for universal tracker

--- worlds/batim/world.py
+++ worlds/batim/world.py
@@ -1,6 +1,8 @@
-from collections.abc import Mapping
-from typing import Any
+import settings
 
+from settings import FilePath
+from collections.abc import Mapping
+from typing import Any, Union, ClassVar, Dict
 from rule_builder.cached_world import CachedRuleBuilderWorld
 # Imports of base Archipelago modules must be absolute.
 from worlds.AutoWorld import World
@@ -9,6 +11,12 @@ from worlds.AutoWorld import World
 from . import items, locations, regions, rules, web_world
 from . import options as batim_options  # rename due to a name conflict with World.options
 
+# Universal Tracker credit to notrainbowsteve on discord
+class UTPackPath(FilePath):
+    required = False
+
+class BATIMSettings(settings.Group):
+    ut_pack_path: Union[UTPackPath, str] = UTPackPath()
 
 class BATIMWorld(World):
     """
@@ -27,6 +35,23 @@ class BATIMWorld(World):
 
     # There is always one region that the generator starts from & assumes you can always go back to.
     # This defaults to "Menu", but you can change it by overriding origin_region_name.
+
+    # Universal Tracker credit to notrainbowsteve on discord
+    @staticmethod
+    def interpret_slot_data(slot_data: Dict[str, Any]) -> Dict[str, Any]:
+        return slot_data
+
+    ut_can_gen_without_yaml = True
+
+    settings_key = "batim_settings"
+    settings: ClassVar[BATIMSettings]
+
+    tracker_world: ClassVar = {
+        "external_pack_key": "ut_pack_path",
+        "map_page_maps": ["maps/maps.json"],
+        "map_page_locations": ["locations/ch1_audio.json", "locations/ch1_bacon.json", "locations/ch1_items.json", "locations/ch2_audio.json", "locations/ch2_bacon.json", "locations/ch2_items.json", "locations/ch3_audio.json", "locations/ch3_bacon.json", "locations/ch3_items.json", "locations/ch4_audio.json", "locations/ch4_bacon.json", "locations/ch4_items.json", "locations/ch5_audio.json", "locations/ch5_bacon.json", "locations/ch5_items.json", "locations/misc.json"],
+        "map_page_layouts": ["layouts/tabs.json"],
+    }
 
     def generate_early(self) -> None:
         batim_options.resolve_option_conflicts(self)
